05f8ca7d6cd2df2aa8fd648cfe9c062539e08bf8
Igor Rodrigo <igrs08@gmail.com>
2026-05-15 20:01:25 -0300

    first commit

---
+++ classe3/src/main/java/com/was/classe3/Classe3Application.java
@@ -0,0 +1,13 @@
+package com.was.classe3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Classe3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Classe3Application.class, args);
+	}
+
+}
